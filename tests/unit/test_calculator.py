73a59d7087e4b8a69230574aaf2f7348d8ae9e1f
declercq sebastien <sdq@afnor.fr>
2020-03-24 11:10:51 +0100

    With params

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -6,8 +6,12 @@
 @date    2020-03-24
 @note    0.0.1 (2020-03-24) : Init file
 '''
+from typing import List, Tuple
 import pytest
 from calculator import Calculator
+
+
+CalculatorValues = Tuple[int, int, int]
 
 
 @pytest.fixture
@@ -15,26 +19,56 @@ def calculator() -> Calculator:
     return Calculator()
 
 
+def add_values() -> List[CalculatorValues]:
+    return [(1, 2, 3), (2, 3, 5), (3, 4, 7)]
+
+
+def sub_values() -> List[CalculatorValues]:
+    return [(2, 1, 1), (1, 2, -1), (1, -2, 3)]
+
+
+def mul_values() -> List[CalculatorValues]:
+    return [(2, 2, 4), (2, 0, 0)]
+
+
+def div_values() -> List[CalculatorValues]:
+    return [(2, 2, 1), (10, 5, 2)]
+
+
+def div_values_float() -> List[CalculatorValues]:
+    return [(1, 2, 0.5), (5, 2, 2.5)]
+
+
 class TestCalculator:
-    def test_add(self, calculator: Calculator) -> None:
-        assert calculator.add(1, 2) == 3
-        assert calculator.add(3, 4) == 7
+    @pytest.mark.parametrize('a,b,result', add_values())
+    def test_add(
+        self, calculator: Calculator, a: int, b: int, result: int
+    ) -> None:
+        assert calculator.add(a, b) == result
 
-    def test_sub(self, calculator: Calculator) -> None:
-        assert calculator.sub(2, 1) == 1
-        assert calculator.sub(1, 2) == -1
-        assert calculator.sub(1, -2) == 3
+    @pytest.mark.parametrize('a,b,result', sub_values())
+    def test_sub(
+        self, calculator: Calculator, a: int, b: int, result: int
+    ) -> None:
+        assert calculator.sub(a, b) == result
 
-    def test_mul(self, calculator: Calculator) -> None:
-        assert calculator.mul(2, 2) == 4
-        assert calculator.mul(2, 0) == 0
+    @pytest.mark.parametrize('a,b,result', mul_values())
+    def test_mul(
+        self, calculator: Calculator, a: int, b: int, result: int
+    ) -> None:
+        assert calculator.mul(a, b) == result
 
-    def test_div_int(self, calculator: Calculator) -> None:
-        assert calculator.div(2, 2) == 1
-        assert calculator.div(10, 5) == 2
+    @pytest.mark.parametrize('a,b,result', div_values())
+    def test_div_int(
+        self, calculator: Calculator, a: int, b: int, result: int
+    ) -> None:
+        assert calculator.div(a, b) == result
 
-    def test_div_float(self, calculator: Calculator) -> None:
-        assert calculator.div(1, 2) == 0.5
+    @pytest.mark.parametrize('a,b,result', div_values_float())
+    def test_div_float(
+        self, calculator: Calculator, a: int, b: int, result: int
+    ) -> None:
+        assert calculator.div(a, b) == result
 
     def test_div_zero_division(self, calculator: Calculator) -> None:
         with pytest.raises(ZeroDivisionError):
